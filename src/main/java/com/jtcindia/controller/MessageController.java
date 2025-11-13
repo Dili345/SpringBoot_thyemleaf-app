1c40bb52b776ade0bd95d177a3cf44cb0352ecd5
Dili345 <kdilip9508@gmail.com>
2025-11-13 13:42:44 +0530

    initial commit

---
+++ src/main/java/com/jtcindia/controller/MessageController.java
@@ -0,0 +1,19 @@
+package com.jtcindia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+
+	@GetMapping("/welcome")
+	public String getWelcomeMsg(Model model) {
+
+		model.addAttribute("msg", "Welcome To Thymeleaf App");
+
+		return "index";
+
+	}
+
+}
