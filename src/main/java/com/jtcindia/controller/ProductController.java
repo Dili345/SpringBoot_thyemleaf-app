e9e5e53128f23db4b01d304685232f05a8446dcd
Dili345 <kdilip9508@gmail.com>
2025-11-13 17:46:16 +0530

    form validation added

--- src/main/java/com/jtcindia/controller/ProductController.java
+++ src/main/java/com/jtcindia/controller/ProductController.java
@@ -2,49 +2,42 @@ package com.jtcindia.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.jtcindia.binding.Product;
 
+import jakarta.validation.Valid;
+
 @Controller
 public class ProductController {
 
 	// method to display form
-	
+
 	@GetMapping("/")
 	public String loadForm(Model model) {
 		model.addAttribute("product", new Product());
-		
+
 		return "index";
-				
-		
+
 	}
-	
-	//method to handle form submission
-	
+
+	// method to handle form submission
+
 	@PostMapping("/product")
-	public String handleSubmitBtn(Product p,Model model) {
-		
+	public String handleSubmitBtn(@Valid Product p, BindingResult result, Model model) {
+
 		System.out.println(p);
-		
-		model.addAttribute("msg", "Product Saved");
-		
+
+		if (result.hasErrors()) {
+			return "index";
+
+		} else {
+
+			model.addAttribute("msg", "Product Saved");
+		}
 		return "index";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
